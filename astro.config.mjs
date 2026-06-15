53d0eb0af13815224ae7bd130ce7724c8c188401
Youtwo_ <ilhamcodes@gmail.com>
2026-06-15 16:29:36 +0700

    configure environment mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+const isProd = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
-  base: "/multiplication",
+  site: "https://kamutwo.github.io",
+  base: isProd ? "/multiplication" : "/",
   vite: {
       plugins: [tailwindcss()],
   },
